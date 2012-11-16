1511ef21e67d29a04550bf01ab51bfe1092684aa
Haacked <haacked@gmail.com>
2012-11-16 14:33:50 -0800

    Add server.js to make this work for Azure
    Note: I'm a node n00b.
    
    I had to rename hubot to hubot.coffee so that the
    require(./bin/hubot) works as it seems Node automatically
    tries to append ".coffee" to the end.
    
    The server.js is the starting point for Azure.

---
+++ server.js
@@ -0,0 +1,5 @@
+// Include the CoffeeScript interpreter so that .coffee files will work
+var coffee = require('coffee-script');
+
+// Include our application file
+var hubot = require('./bin/hubot');
